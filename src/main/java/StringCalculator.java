540bfede41619947cab07f1b2077b7acf55fff01
vinod <egoistvinod@gmail.com>
2024-12-14 12:59:01 +0530

    Support custom delimiters

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,15 +1,21 @@
 public class StringCalculator {
-
     public int add(String numbers) {
         if (numbers.isEmpty()) {
             return 0;
         }
-        String[] numArray = numbers.split(",|\n");
+        String delimiter = ",|\n";
+        if (numbers.startsWith("//")) {
+            int delimiterIndex = numbers.indexOf("\n");
+            delimiter = numbers.substring(2, delimiterIndex);
+            numbers = numbers.substring(delimiterIndex + 1);
+        }
+        String[] numArray = numbers.split(delimiter);
         int sum = 0;
         for (String num : numArray) {
             sum += Integer.parseInt(num);
         }
         return sum;
     }
+
 
 }
