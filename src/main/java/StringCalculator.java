caa60b7337034987a1deb525ac0d80e4361c11ec
vinod <egoistvinod@gmail.com>
2024-12-14 12:57:09 +0530

    Handle newline as delimiter

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,13 +1,15 @@
 public class StringCalculator {
+
     public int add(String numbers) {
         if (numbers.isEmpty()) {
             return 0;
         }
-        String[] numArray = numbers.split(",");
+        String[] numArray = numbers.split(",|\n");
         int sum = 0;
         for (String num : numArray) {
             sum += Integer.parseInt(num);
         }
         return sum;
     }
+
 }
